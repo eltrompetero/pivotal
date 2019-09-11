aa1ebe2fbffb2ed3b930c60189a6c7c9f5fd037e
Eddie Lee <el.trompetero@gmail.com>
2019-09-11 00:56:53 -0400

    ready for testing

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author: Eddie Lee, edl56@cornell.edu
+
+# Run the following commands in your shell prompt to create custom Python environment.
+# conda create --name <env_name>
+# conda activate <env_name>
+
+echo 'We recommend creating a custom Python environment first. Open setup.sh for instructions.'
+read -p 'Are you sure want to install new packages into your current Python environment? [Yn]' yn
+    case $yn in
+        [Yy]* ) ./install.sh;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+
+echo 'To run code in Jupyter notebook, follow instructions on ConIII homepage for creating'
+echo 'files for 10 spin systems.'
